aa743aeb31da7624aeda3d37b27cc315630a49b5
PadsterH2012 <paddy@bastiondata.com>
2019-11-16 09:57:48 +0000

    Update mkwp.sh

--- mkwp.sh
+++ mkwp.sh
@@ -122,14 +122,14 @@ include /etc/nginx/conf.d/*.conf;
 server {
         listen 80 default_server;
         listen [::]:80 default_server;
-        server_name 35.204.8.247;
+        server_name test.com;
         return 301 https://$server_name$request_uri;
 }
 
 server {
 listen       443 ssl http2 default_server;
 listen       [::]:443 ssl http2 default_server;
-server_name  35.204.8.247s;
+server_name  test.coms;
 root         /usr/share/nginx/html;
 
 ssl_certificate "/etc/pki/nginx/server.crt";
